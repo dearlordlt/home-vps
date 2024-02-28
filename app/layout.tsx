367dbc287fcfa015f61995ea5e51df88f75875d2
dearlordlt <sarunas79@gmail.com>
2024-02-28 15:18:40 +0000

    feat: icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <link rel="icon" href="/favicon.ico" sizes="any" />
       <body>{children}</body>
     </html>
   )
